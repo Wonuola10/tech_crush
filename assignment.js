d426a5b1f36f356fa096e6c66e843f334f4c6bf8
Wonuola10 <wonuolashonde@email.com>
2026-04-08 18:38:34 -0700

    Assignment on variables, operators and data types

---
+++ assignment.js
@@ -0,0 +1,41 @@
+//ASSIGNMENT 1
+//Create Variables
+let customerName = "Esther"
+let customerAge = 24
+let bookTitle = "Javascript Basics"
+let bookPrice = 5000
+let quantity = 3
+let isStudent = "True or False"
+
+//ASSIGNMENT 2
+//USING ARITHMETIC OPERATORS 
+let totalPrice;
+totalPrice = bookPrice * quantity
+
+
+//ASSIGNMENT 3
+//Use Comparison Operators
+let isAdult;
+let buyingManyBooks;
+let isBookFiveThousand
+isAdult = customerAge >= 18
+buyingManyBooks = quantity < 3
+isBookFiveThousand = bookPrice == 5000
+
+
+//ASSIGNMENT 4
+// Using Non-primitive Data Type
+const customerOrder = {
+    customerName: "Esther",
+    bookTitle: "Javascript Basics",
+    quantity: 50,
+    totalPrice: 5000
+}
+//Task 5
+//Print Results 
+console.log(customerName)
+console.log(bookTitle)
+console.log(totalPrice)
+console.log(isAdult)
+console.log(buyingManyBooks)
+console.log(isBookFiveThousand)
