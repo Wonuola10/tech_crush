8ad5af88e19a0439df8ed31fec368b2ed88daf9a
Wonuola10 <wonuolashonde@email.com>
2026-04-08 18:45:26 -0700

    Variable.js

---
+++ variable.js
@@ -0,0 +1,33 @@
+//
+
+var myName = "Esther"
+//console.log (myName)
+let myAge = 30
+const myHeight = 5.7
+//var, you can u
+//var myName = "John"
+//console.log (myName)
+
+
+//vari
+myName = "Adina"
+console.log (myName)
+
+myAge = 31
+console.log (myAge)
+
+myHeight = 5.7
+console.log (myHeight)
+
+
+//Primitive Data Types: String, Number, Boolean, Null, Underdefined
+let myString = "Hello, World!"
+let myNumber = 42
+let myBoolean = true
+console.log (myString)
+console.log (myNumber)
+console.log (myBoolean)
+
+//Non-primitive Data Types: Object, Array, Function
+let myObject = {name: "John", age: 30 , isStudent: true}
+let myArray = []
