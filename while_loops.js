e99de2b2527c42e70b70218a16edae1347d6f1bd
Wonuola10 <wonuolashonde@email.com>
2026-04-08 18:45:47 -0700

    While_loops.js

---
+++ while_loops.js
@@ -0,0 +1,14 @@
+let tries = 0
+while(tries < 3){
+    console.log( tries)
+    tries++
+}
+//Run 3 times, then stops because attempts is no longer less than 3ß
+
+
+let count = 10
+while (count >= 1){
+    console.log(count)
+    count-- 
+}
+console.log("HAPPY NEW YEAR !")
